a73f8377922d0261a6456f845fca7f7f1300661b
NguyenThu1996 <36024045+NguyenThu1996@users.noreply.github.com>
2019-05-22 17:00:11 +0700

    commit
    addd

---
+++ OrientSoftWare.Base.My/core/DataAccess/Repository/Base/Repository.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using OrientSoftWare.Base.My.Entities;
+
+namespace OrientSoftWareBaseMy.core.DataAccess.Repository.Base
+{
+    public class Repository<T> : IRepository<T> where  T : BaseEntity 
+    {
+        public Task<ResponseModel> DeleteAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel> DeleteAsync(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel> DeleteAsync(IEnumerable<Guid> ids)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel> DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel> ExcuteSqlAsync(string sql)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> Fetch(Expression<Func<T, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> FetchFirstAsync(Expression<Func<T, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(Guid? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OrientNetCoreContext GetDbContext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel> InsertAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel> InsertAsync(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel> SetRecordInactiveAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel> UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
